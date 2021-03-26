22036d463c19da8c2e1d1bf793128ededd83dcba
sasikumarkeskaran <78546975+sasikumarkeskaran@users.noreply.github.com>
2021-03-26 12:40:45 +0530

    Add files via upload

---
+++ pyramid star pattern.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+   int no,i,j,k,c=80;
+   printf("Enter the  no of rows you want:");
+   scanf("%d",&no);
+   for(i=1;i<=no;i++)
+   {
+       for(j=1;j<=c/2-i;j++)
+       {
+           printf(" ");
+       }
+       for(k=1;k<=2*i-1;k++)
+       {
+           printf("*");
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
